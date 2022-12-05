9847459a12c0fd5d516ba1c129b257c4f6dcdf4a
yrk <yangruikai@mail.dlut.edu.cn>
2022-12-05 20:47:32 +0800

    YRK 2022-12-5 Correcting description "Super parameter" --> "Hyper parameter"

--- train.py
+++ train.py
@@ -9,11 +9,11 @@ from train_and_test import train
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train')
-    parser.add_argument('--alpha', default=0.1, type=float, help='Alpha super-parameter which controls Inter-class '
+    parser.add_argument('--alpha', default=0.1, type=float, help='Alpha hyper-parameter which controls Inter-class '
                                                                  'Dispersion Metric')
-    parser.add_argument('--gamma', default=0.5, type=float, help='Beta super-parameter which controls Intra-class '
+    parser.add_argument('--gamma', default=0.5, type=float, help='Beta hyper-parameter which controls Intra-class '
                                                                  'Interaction Metric')
-    parser.add_argument('--beta', default=0.5, type=float, help='Gamma super-parameter which controls Intra-class '
+    parser.add_argument('--beta', default=0.5, type=float, help='Gamma hyper-parameter which controls Intra-class '
                                                                 'Compactness Metric')
     parser.add_argument('--save_path', default='./save', type=str, help='Directory to save trained model')
     parser.add_argument('--train_path', default='./data/DOTA_RSOR/train', type=str, help='Data to train the model')
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     parser.add_argument('--initial_resnet_path', default='./Ready Model/resnet50-pretrained.pth', type=str,
                         help='Initial model parameters')
     args = parser.parse_args()
-    print(f"Super parameters: "
+    print(f"Hyper parameters: "
           f"Alpha {args.alpha}, Beta {args.beta}, Gamma {args.gamma}.\n"
           f"Save trained model to {args.save_path}.\n"
           f"Train data from {args.train_path}, Eval data from {args.test_path}.\n"
